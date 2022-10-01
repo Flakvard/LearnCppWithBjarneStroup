54b254ce4602ae7490795a8fb87ebdd8d85ff3df
Marni <marni@falkvard.com>
2022-10-01 12:55:34 +0200

    execises

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,29 @@
 #include "./std_lib_facilities.h"
 
-int main() //C++ programs start by executing the function main
+int main() // C++ programs start by executing the function main
 {
-    cout <<"Hello Marni and the World!\n"; //cout= character output = "Hello, World"
+    cout << "Hello, programming!\nHere we go!\n"; // cout= character output = "Hello, World"
+    /* this is step 5 in the exercises:*/
+    cout << "// - the compiler skips these character, until the next line. You can comment after the '//' - double slash \n"; 
+    cout << "<< - is a output operator\n";
+    cout << "C++ - this is the language I am learning\n";
+    cout << "Comment - you can comment after the // operators or /* */ for many lines of code\n";
+    cout << "Compiler - the compiler compiles the code into object code or machine code\n";
+    cout << "Compile-time error - this is when there is an error in the source code, aka the code you are writing\n";
+    cout << "cout - stands for character(c) output(out) pronaunced \"see out\" and prints out the characters after the << output operator\n";
+    cout << "executable - is a file that can be executed f.ex. a .exe file\n";
+    cout << "function - is forexample 'main(){}' where the machine starts its first function. Other examples of functions are functions from the .h file \n";
+    cout << "header - this is a .h file which stores libraries for example. This gives the code more functions\n";
+    cout << "IDE - stands for interactive development enviorment. It helps a programmer with debug, syntacts highlighting and other stuff not so compatible on the CLI\n";
+    cout << "#include - this function includes other libraries into the code you are trying to execute. F.ex. std_lib_facilities to execute 'cout' \n";
+    cout << "library - this is a code with a bunch of functions made by other programmers and helps you execute your program and makes your code efficent \n";
+    cout << "linker - The linker links the .obj/.o file into an .exe in windos and in linux none (you turn the file into a rwe file, (r)ead (w)rite (e)xecture)\n";
+    cout << "main() this is the functions that always has to be in a cpp program, it tells the compiler where to start\n";
+    cout << "object code - same as machine code and it spit out of the compiler, if there was no compile-time error\n";
+    cout << "output - is the output you want for a file name forex. It could also be the << that tells the compiler where to output the characters in \" \" \n";
+    cout << "program - Defined as \"What to do\". This is the .exe file if there was no link-time error. If we run the program .exe and there is an error it is called run-time error og logic error\n";
+    cout << "source code - this the what usually is referred to as code. This is the code that the programmer writes \n";
+    cout << "statement - statement is a line of code to be executed - usually stops after a ;. The compiler needs the ; to know where to stop. Declare and int, execture a function, print a line ect \n";
+    keep_window_open();
     return 0;
 }
