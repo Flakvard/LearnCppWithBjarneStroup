d4a321039ab6b8571bb5e1b48a46d47d297ba80d
flakvard <marni@falkvard.com>
2022-10-19 10:21:21 +0200

    exercies 5 .

---
+++ Chapter_5/5_6_exercises.cpp
@@ -0,0 +1,17 @@
+#include "../std_lib_facilities.h"
+
+
+double ctok(double c);
+
+int main()
+{
+  double c = 0; //declare input variable
+  cin>>d;       //retrieve temperature to input variable
+  double k = ctok("c"); //convert temperature
+  cout<<k<<'\n';        //print out temperature
+}
+
+double ctok(double c)
+{
+  int k= c
+}
