4f47f47cfb76c032ce1c94ba78cbeec54484b7a2
Marni <marni@falkvard.com>
2023-06-13 22:52:56 +0200

    Chapter_8

---
+++ Chapter_8/my.cpp
@@ -0,0 +1,13 @@
+#include <my.h>
+#include "../std_lib_facilities.h"
+
+
+void print(int n){
+    for(int i = 0; i < n; i++){
+        cout<<i<<'\n';
+        }
+    }
+    
+void print_foo(){
+        cout<<"foo\n";
+    }
---
+++ Chapter_8/use.cpp
@@ -0,0 +1,11 @@
+
+#include "./my.h"
+
+int main(int argc, char* argv[]){
+
+    // foo = 7;
+
+    print_foo();
+    print(5);
+
+}
