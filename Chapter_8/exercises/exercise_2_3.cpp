4ecc57c130e65a81848237f76b369f6577e58286
flakvard <marnifalkvard@gmail.com>
2023-06-27 13:31:30 +0100

    exercise 3 fibonacci

--- Chapter_8/exercises/exercise_2_3.cpp
+++ Chapter_8/exercises/exercise_2_3.cpp
@@ -16,14 +16,16 @@ sequence starting with its x and y arguments
 #include "../../std_lib_facilities.h"
 
 void print(vector<int>& vec, string& str);
-fibonacci(int x, int y, vector<int> v, int n);
+void fibonacci(int x, int y, vector<int> v, size_t n);
 
 
 int main(){
     vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
+    vector<int> v;
     string label = "Hello number ";
 
-    print(vec,label);
+    // print(vec,label);
+    fibonacci(10,20,v,5);
 
 }
 
@@ -32,8 +34,23 @@ void print(vector<int>& vec, string& str){
         cout<<str<<vec[i]<<'\n';
     }
 }
-fibonacci(int x, int y, vector<int> v, int n){
-    for(size_t i = 0; i < v.size(), i++){
-        
+void fibonacci(int x, int y, vector<int> v, size_t n){
+    int fib = 0;
+    v.push_back(x);
+    v.push_back(y);
+
+    if(n == 1 || n == 2){
+        fib = x+y;
+        v.push_back(fib);
+    }
+    else{
+        for(size_t i = 1; i < n; i++){
+            fib = v[i-1] + v[i];
+            v.push_back(fib);
+        }
+    }
+    string label = "Hello fibo number: ";
+    for(size_t i = 0; i < v.size(); i++){
+        cout<<label<<v[i]<<'\n';
     }
 }
