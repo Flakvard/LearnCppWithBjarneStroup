55ba0fda87ffb248f916dcfd48fafab7d4725500
flakvard <marnifalkvard@gmail.com>
2023-06-28 09:32:28 +0100

    exerccise 6

--- Chapter_8/exercises/exercise_2_3_4_5.cpp
+++ Chapter_8/exercises/exercise_2_3_4_5.cpp
@@ -17,6 +17,7 @@
     For example, 1, 3, 5, 7, 9 becomes 9, 7, 5, 3, 1. The first reverse function 
     should produce a new vector with the reversed sequence, leaving its original vector unchanged.
     The other reverse function should reverse the elements of its vector without using any other vectors (hint: swap).
+6.  Write versions of the functions from exercise 5, but with a vector<string>.
 
 */
 #include "../../std_lib_facilities.h"
@@ -25,20 +26,40 @@ void print(vector<int>& vec, string& str);
 void fibonacci(int x, int y, vector<int> v, size_t n);
 void reverseVec(vector<int> vec);
 void swapVec(vector<int>& vec);
+void swapStrVec(vector<string>& vec);
+void printStr(vector<string>& vec, string& str);
 
 int main(){
     vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
+    vector<string> vecStr = {"hello", "my", "name", "is", "unnamed"};
     vector<int> v;
     string label = "Hello normal vec ";
 
-    print(vec,label);
+    // print(vec,label);
     // fibonacci(10,20,v,50);
-    reverseVec(vec);
-    swapVec(vec);
-
-
-
+    // reverseVec(vec);
+    // swapVec(vec);
+    printStr(vecStr, label);
+    swapStrVec(vecStr);
 }
+void swapStrVec(vector<string>& vec){
+    // Brute force way
+    for(size_t i = 1; i < vec.size(); i++){
+        for(size_t k = 1; k < vec.size(); k++){
+            if (vec[k-1]<vec[i]){
+                string temp; 
+                temp = vec[k-1];
+                vec[k-1] = vec[i]; 
+                vec[i] = temp; 
+            }
+        }
+    }
+    // inbuilt sort way
+    // sort(vec.begin(),vec.end(),greater());
+    string label = "Hello reverse vec ";
+    printStr(vec,label);
+}
+
 
 void swapVec(vector<int>& vec){
     // Brute force way
@@ -67,6 +88,12 @@ void reverseVec(vector<int> vecCopy){
 }
 
 void print(vector<int>& vec, string& str){
+    for(size_t i = 0; i < vec.size(); i++){
+        cout<<str<<vec[i]<<'\n';
+    }
+}
+
+void printStr(vector<string>& vec, string& str){
     for(size_t i = 0; i < vec.size(); i++){
         cout<<str<<vec[i]<<'\n';
     }
