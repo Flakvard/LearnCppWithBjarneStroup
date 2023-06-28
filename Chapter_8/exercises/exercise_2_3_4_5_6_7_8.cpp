4937845937b1ee939b9f0ff4dc2b658fa522a7a0
flakvard <marnifalkvard@gmail.com>
2023-06-28 10:32:09 +0100

    exercise 7 and 8

--- Chapter_8/exercises/exercise_2_3_4_5_6_7_8.cpp
+++ Chapter_8/exercises/exercise_2_3_4_5_6_7_8.cpp
@@ -18,7 +18,13 @@
     should produce a new vector with the reversed sequence, leaving its original vector unchanged.
     The other reverse function should reverse the elements of its vector without using any other vectors (hint: swap).
 6.  Write versions of the functions from exercise 5, but with a vector<string>.
-
+7.  Read five names into a vector<string> name, then prompt the user for the ages of the people named and store the
+    ages in a vector<double> age. Then print out the five (name[i],age[i]) pairs.
+    Sort the names (sort(name.begin(),name.end())) and print out the (name[i],age[i]) pairs.
+    The tricky part here is to get the age vector in the correct order to match the sorted name vector.
+    Hint: Before sorting name, take a copy and use that to make a copy of age in the right order after 
+    sorting name.
+ 8. Then, do that exercise again but allowing an arbitrary number of names
 */
 #include "../../std_lib_facilities.h"
 
@@ -28,6 +34,8 @@ void reverseVec(vector<int> vec);
 void swapVec(vector<int>& vec);
 void swapStrVec(vector<string>& vec);
 void printStr(vector<string>& vec, string& str);
+void printVecStrAndInt(vector<string>& vecStr, vector<int>& vecNum);
+void SortPairStrVec(vector<string>& name, vector<int>& age);
 
 int main(){
     vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
@@ -35,13 +43,42 @@ int main(){
     vector<int> v;
     string label = "Hello normal vec ";
 
+    // exercise 2
     // print(vec,label);
+
+    // exercise 4+3
     // fibonacci(10,20,v,50);
+    
+    // exercise 5
     // reverseVec(vec);
     // swapVec(vec);
-    printStr(vecStr, label);
-    swapStrVec(vecStr);
+
+    // exercise 6
+    // printStr(vecStr, label);
+    // swapStrVec(vecStr);
+
+    // exercise 7
+    vector<int> age = {25,12,20,18,40};
+    vector<string> name = {"Carol", "Charles", "Victor", "Abby", "Maggi"};
+    printVecStrAndInt(name, age);
+    SortPairStrVec(name, age);
+    // sort(name.begin(),name.end());
+    // printVecStrAndInt(name, age);
 }
+
+void SortPairStrVec(vector<string>& name, vector<int>& age){
+    vector<string> nameCopy = name;
+    sort(name.begin(),name.end());
+    // swapStrVec(name);
+    for(size_t k = 0; k < name.size(); k++){
+        for(size_t i = 0; i < nameCopy.size(); i++){
+            if (name[k] == nameCopy[i]){
+                cout<<name[k]<<" is age "<<age[i]<<'\n';
+            }
+        }
+    }
+}
+
 void swapStrVec(vector<string>& vec){
     // Brute force way
     for(size_t i = 1; i < vec.size(); i++){
@@ -57,7 +94,7 @@ void swapStrVec(vector<string>& vec){
     // inbuilt sort way
     // sort(vec.begin(),vec.end(),greater());
     string label = "Hello reverse vec ";
-    printStr(vec,label);
+    // printStr(vec,label);
 }
 
 
@@ -96,6 +133,14 @@ void print(vector<int>& vec, string& str){
 void printStr(vector<string>& vec, string& str){
     for(size_t i = 0; i < vec.size(); i++){
         cout<<str<<vec[i]<<'\n';
+    }
+}
+
+void printVecStrAndInt(vector<string>& vecStr, vector<int>& vecNum){
+    if(vecStr.size()!=vecNum.size()) cout<<"Error not same size";
+
+    for(size_t i = 0; i < vecNum.size(); i++){
+        cout<<vecStr[i]<<" is age "<<vecNum[i]<<'\n';
     }
 }
 void fibonacci(int x, int y, vector<int> v, size_t n){
