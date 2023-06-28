e73d13b9d8879f617922f56a608e69b30da6e843
flakvard <marnifalkvard@gmail.com>
2023-06-28 14:48:39 +0100

    exercise 12 complete

--- Chapter_8/exercises/exercise_12.cpp
+++ Chapter_8/exercises/exercise_12.cpp
@@ -4,6 +4,31 @@
     sees a second occurrence of its quit argument
 */
 
-int main(int argc, int* argv[]){
+#include "../../std_lib_facilities.h"
+void print_until_s(vector<string> v, string quit);
+void print_until_ss(vector<string> v, string quit, int times);
 
+int main(int argc, char* argv[]){
+    vector<string> strings = {"Hello", "World", "Just", "Quit","Or", "Don't", "Give", "Up","And", "Quit"};
+    string quit = "Quit";
+    print_until_ss(strings, quit, 2);
+    // print_until_s(strings, quit);
+}
+
+void print_until_s(vector<string> v, string quit)
+{
+    for(string s : v) {
+        if (s==quit) return;
+        cout << s << '\n';
+    }
+}
+
+void print_until_ss(vector<string> v, string quit, int times)
+{
+    int flag = 0;
+    for(string s : v) {
+        if (s==quit) flag += 1;
+        cout << s << '\n';
+        if (flag==times) return;
+    }
 }
