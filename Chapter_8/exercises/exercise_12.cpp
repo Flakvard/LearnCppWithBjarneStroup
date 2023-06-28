ca1331c1ca1b89cc26282a240370c7515c655078
flakvard <marnifalkvard@gmail.com>
2023-06-28 14:15:30 +0100

    update

---
+++ Chapter_8/exercises/exercise_12.cpp
@@ -0,0 +1,9 @@
+/*
+    Improve print_until_s() from §8.5.2. Test it. What makes a good set of 
+    test cases? Give reasons. Then, write a print_until_ss() that prints until it 
+    sees a second occurrence of its quit argument
+*/
+
+int main(int argc, int* argv[]){
+
+}
