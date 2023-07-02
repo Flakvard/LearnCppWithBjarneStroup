ff83fa2caa9dbeb5e1412eef4eba17f7d48d11b5
flakvard <marnifalkvard@gmail.com>
2023-07-02 11:54:08 +0100

    all versions done

--- Chapter_9/drill_3.cpp
+++ Chapter_9/drill_3.cpp
@@ -11,7 +11,6 @@ that is not in the [1,31] range. Test each version with at least one invalid dat
  3. The version from §9.4.3
  4. The version from §9.7.1
  5. The version from §9.7.4
-
 */
 
 #include "../std_lib_facilities.h"
@@ -26,11 +25,25 @@ public:
     class Invalid {};
     Year(int y);
     int year(){return y;}
+    void add_year(int n);
     //~Year();
 private:
     static const int min = 1800;
     static const int max = 2200;
     int y;
+};
+
+class Day
+{
+public:
+    class Invalid {};
+    Day(int day);
+    int day() const {return d;}
+    void add_day(int n);
+private:
+    int d;
+    static const int max = 31;
+    static const int min = 1;
 };
 
 
@@ -40,13 +53,16 @@ class Date
 public:
     Date();
     //Date(Year y);
-    Date(int d, Month m, Year y);
-    void add_day(int n);
-    int day()const{return d;}
+    Date(Day d, Month m, Year y);
+    Day day()const{return d;}
     Month month()const{return m;}
     Year year()const{return y;}
+
+    void add_day(int n);
+    void add_month(int n);
+    void add_year(int n);
 private:
-    int d;
+    Day d;
     Month m;
     Year y;
 };
@@ -54,34 +70,45 @@ private:
 const Date& default_date();
 ostream& operator<<(ostream& os, const Date& d);
 
+Day::Day(int day){
+    if (day > max || day < min){
+        Invalid();
+    }else
+    {
+        d = day;
+    }
+    
+}
+
 
 int main(int argc, char* argv[]){
     cout<<argc<<'\n';
     cout<<*argv<<'\n';
     // June 25, 1978.
 
-    Date today(25, Month::jun, Year(1978));
+    Date today(Day(25), Month::jun, Year(1978));
+    cout<<today<<'\n';
+    today.add_month(7);
+    today.add_year(2);
+    cout<<today<<'\n';
+
     Date defaultDate;
-    cout<<defaultDate.day()<<'\n';
+    cout<<defaultDate<<'\n';
+
     vector<Date>hello(10);
     for (size_t i = 0; i < 10; i++)
     {
         cout<<hello[i]<<'\n';
     }
     
-    //cout<<defaultDate.month()<<'\n';
-    //cout<<defaultDate.year()<<'\n';
-    //cout<<today.day()<<'\n';
+    cout<<today<<'\n';
     today.add_day(1);
-    // cout<<today.day()<<'\n';
-    // cout<<today.year()<<'\n'; TODO: FIX
-    // cout<<today.month()<<'\n'; TODO: fix
+    cout<<today<<'\n';
     
 }
 
-// Date::Date() :d(1), m(Month::jan), y(2000) {};
 
-Date::Date(int day, Month month, Year year) : d(day), m(month), y(year){}
+Date::Date(Day day, Month month, Year year) : d(day), m(month), y(year){}
 
 
 Date::Date()
@@ -92,7 +119,38 @@ Date::Date()
 
 
 void Date::add_day(int n){
-    Date::d += n;
+    //Date::d += n;
+    d.add_day(n);
+}
+
+void Day::add_day(int n)
+{
+    if(d < min|| d > max){
+        throw Invalid{};
+    }else{
+        d += n;
+    }
+}
+
+void Date::add_month(int n){
+    // int size = n+int(m);
+    int size = static_cast<int>(m) + n;
+    if (size >= static_cast<int>(Month::des))
+        m = Month::jan;
+    else{
+        m = static_cast<Month>(size);
+    };
+}
+void Date::add_year(int n){
+    y.add_year(n);
+}
+void Year::add_year(int n){
+    int size = y+n;
+    if(size <min || size > max){
+        throw Invalid{};
+    }else{
+        y += n; 
+    }
 }
 
 Year::Year(int year) : y(year){
@@ -102,13 +160,13 @@ Year::Year(int year) : y(year){
 
 const Date &default_date()
 {
-    static Date dd(1, Month::jan, Year(2001));
+    static Date dd(Day(1), Month::jan, Year(2001));
     return dd;
 }
 
 
 ostream& operator<<(ostream& os, const Date& d){
-    return os<<'('<<d.day()
+    return os<<'('<<d.day().day()
              <<','<<int(d.month())
              <<','<<d.year().year()<<')';
 }
